d4e54bbaca841a022c4a15e339a2b39e9c93146d
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2022-09-09 17:04:10 +0200

    Map stuff

--- Mutually Assured Destruction/Assets/Scripts/Map/MapGenerator.cs
+++ Mutually Assured Destruction/Assets/Scripts/Map/MapGenerator.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Map currentMap;
+    [HideInInspector] Map currentMapCheck;
+    public Texture2D terrainMap;
+
+
+    private void OnValidate()
     {
-        
+        if (currentMap != currentMapCheck)
+        {
+            currentMapCheck = currentMap;
+
+            if (currentMap == null)
+            {
+                terrainMap = null;
+            }
+            else
+            {
+                terrainMap = currentMap.terrainMap;
+            }
+        }
     }
 }
